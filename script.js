974359c6ac2a568da680d2e77fdf43947cc15b09
Nicolas Melo <nunes.melo18@gmail.com>
2023-12-28 10:31:58 -0300

    Feito a marcação no HTML e estilização com CSS

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+-function(operations) = divisão => onclick && onkeypress 
+-function(clear) = limpar => onclick && onkeypress 
+-function(display) = resultado => onclick && onkeypress 
+-function(numbers) = numeros => onclick && onkeypress 
+-function(calculateResult)
+*/
+
+let operations 
+const clear = document.getElementById(clear)
+const display = document.getElementById(display)
+let numbers = document.getElementsByClassName(numbers)
+const calculateResult = document.getElementById(calculateResult)
+
